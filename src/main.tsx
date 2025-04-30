f8df144a4d882fa92473bb8d1d946d1eb4c02f95
tokosutomo123@gmail.com <tokosutomo123@gmail.com>
2025-04-30 09:25:54 +0700

    remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import "./index.css"; 
+import "./index.css";
 
-// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Create a new router instance
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
@@ -19,7 +16,6 @@ declare module "@tanstack/react-router" {
 
 const queryClient = new QueryClient();
 
-// Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
